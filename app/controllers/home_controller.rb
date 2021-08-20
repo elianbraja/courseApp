3fba81bbfe69c5542ffa729e51e009128787f946
Elian Braja <elianbraja@gmail.com>
2021-08-20 15:45:34 +0200

    styling and organising

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
+  skip_before_action :authenticate_user!, :only => [:index]
   def index
+    @courses = Course.all.limit(3)
+    @recent_courses = Course.all.limit(3).order(created_at: :desc)
+    @latest_courses = Course.all.limit(3).order(created_at: :asc)
   end
 end
