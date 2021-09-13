7e88cb0c23752c8df08144f7fa02daab86cdf94f
Elian Braja <elianbraja@gmail.com>
2021-09-13 20:03:39 +0200

    Rolify and Pundit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,12 +8,31 @@ class User < ApplicationRecord
 
   has_many :courses
 
-  def to_s
-    email
-  end
+  after_create :assign_default_role
 
   def username
-    self.email.split(/@/).first
+    email.split(/@/).first
+  end
+
+  def assign_default_role
+    if User.count == 1
+      add_role(:admin) if self.roles.blank?
+      add_role(:teacher)
+      add_role(:student)
+    else
+      add_role(:student) if self.roles.blank?
+      add_role(:teacher)
+    end
+  end
+
+  validate :must_have_a_role, on: :update
+
+  private
+
+  def must_have_a_role
+    unless roles.any?
+      errors.add(:roles, "must have at least one role")
+    end
   end
 
 end
